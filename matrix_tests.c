86bc53b0e34ef73c31695a5b8f61f9f0e22f9754
Carson James <carsonaj@stat.tamu.edu>
2021-01-12 10:16:08 -0600

    added tests for mat_prod, mat_had_prod, mat_sum, mat_transpose

--- matrix_tests.c
+++ matrix_tests.c
@@ -12,7 +12,6 @@
 // data structure: 
 
 void test_mat_create() {
-    Matrix *mat = mat_create(7, 8);
     Matrix *mat2 = mat_create(2, 3);
     assert(mat2->rows == 2);
     assert(mat2->cols == 3);
@@ -48,6 +47,8 @@ void test_mat_fill() {
     for (i=1; i<6; i++) {
         assert(mat->data[i] = i+1);
     }
+
+    mat_delete(mat);
 
     return;
 }
@@ -101,8 +102,8 @@ void test_mat_get_rows() {
 
 void test_mat_get_cols() {
     Matrix *m = mat_create(3, 3);
-    Matrix *cm = mat_create(2, 3);
-    Matrix *cmc = mat_create(2, 3);
+    Matrix *cm = mat_create(3, 2);
+    Matrix *cmc = mat_create(3, 2);
 
     double m_entries[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     double cmc_entries[6] = {1, 2, 4, 5, 7, 8};
@@ -131,7 +132,10 @@ void test_mat_row_op1() {
     mat_fill(cmat, centries);
 
     mat_row_op1(mat, 1, 2);
-    assert(mat_equal(cmat, mat));
+    assert(mat_equal(cmat, mat) == TRUE);
+
+    mat_delete(mat);
+    mat_delete(cmat);
 
     return;
 }
@@ -145,7 +149,10 @@ void test_mat_row_op2() {
     mat_fill(cmat, centries);
 
     mat_row_op2(mat, 1, 2);
-    assert(mat_equal(cmat, mat));
+    assert(mat_equal(cmat, mat) == TRUE);
+
+    mat_delete(mat);
+    mat_delete(cmat);
 
     return;
 }
@@ -154,16 +161,132 @@ void test_mat_row_op3() {
     Matrix *mat = mat_create(3, 3);
     Matrix *cmat = mat_create(3, 3);
     double entries[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    double centries[9] = {1, 2, 3, -3, -3, -3, 4, 5, 6};
+    double centries[9] = {1, 2, 3, -3, -3, -3, 7, 8, 9};
     mat_fill(mat, entries); 
     mat_fill(cmat, centries);
 
     mat_row_op3(mat, 1, 2, -1);
-    assert(mat_equal(cmat, mat));
+    assert(mat_equal(cmat, mat) == TRUE);
+
+    mat_delete(mat);
+    mat_delete(cmat);
 
     return;
 }
 
+void test_mat_product() {
+    Matrix *m1 = mat_create(2,3);
+    Matrix *m2 = mat_create(3,2);
+    Matrix *p = mat_create(2,2);
+    Matrix *pc = mat_create(2,2);
+
+    double ent1[6] = {1, 1, 1, 2, 2, 2};
+    double ent2[6] = {1, 1, 2, 2, 3, 3};
+    double entc[4] = {6, 6, 12, 12};
+
+    mat_fill(m1, ent1);
+    mat_fill(m2, ent2);
+    mat_fill(pc, entc);
+
+    mat_product(m1, m2, p);
+    assert(mat_equal(p, pc) == TRUE);
+
+    mat_delete(m1);
+    mat_delete(m2);
+    mat_delete(p);
+    mat_delete(pc);
+
+    return;
+}
+
+void test_mat_had_product() {
+    Matrix *m1 = mat_create(2,2);
+    Matrix *m2 = mat_create(2,2);
+    Matrix *p = mat_create(2,2);
+    Matrix *pc = mat_create(2,2);
+
+    double ent1[4] = {1, 1, 2, 2};
+    double ent2[4] = {3, 3, -1, -1};
+    double entc[4] = {3, 3, -2, -2};
+
+    mat_fill(m1, ent1);
+    mat_fill(m2, ent2);
+    mat_fill(pc, entc);
+
+    mat_had_product(m1, m2, p);
+    assert(mat_equal(p, pc) == TRUE);
+
+    mat_delete(m1);
+    mat_delete(m2);
+    mat_delete(p);
+    mat_delete(pc);
+
+    return;
+}
+
+void test_mat_scale() {
+    Matrix *m = mat_create(2,2);
+    double k = -2;
+    Matrix *mc = mat_create(2,2);
+
+    double ent[4] = {1, 1, 2, 2};
+    double entc[4] = {-2, -2, -4, -4};
+
+    mat_fill(m, ent);
+    mat_fill(mc, entc);
+
+    mat_scale(k, m);
+    assert(mat_equal(m, mc) == TRUE);
+
+    mat_delete(m);
+    mat_delete(mc);
+
+    return;
+}
+
+void test_mat_sum() {
+    Matrix *m1 = mat_create(2,2);
+    Matrix *m2 = mat_create(2,2);
+    Matrix *p = mat_create(2,2);
+    Matrix *pc = mat_create(2,2);
+
+    double ent1[4] = {1, 4, 2, 2};
+    double ent2[4] = {3, 3, -1, -1};
+    double entc[4] = {4, 7, 1, 1};
+
+    mat_fill(m1, ent1);
+    mat_fill(m2, ent2);
+    mat_fill(pc, entc);
+
+    mat_sum(m1, m2, p);
+    assert(mat_equal(p, pc) == TRUE);
+
+    mat_delete(m1);
+    mat_delete(m2);
+    mat_delete(p);
+    mat_delete(pc);
+
+    return;
+}
+
+void test_mat_transpose() {
+    Matrix *m = mat_create(2,2);
+    Matrix *mc = mat_create(2,2);
+
+    double ent[4] = {1, 4, 3, -1};
+    double entc[4] = {1, 3, 4, -1};
+
+    mat_fill(m, ent);
+    mat_fill(mc, entc);
+
+    mat_transpose(m);
+    assert(mat_equal(m, mc) == TRUE);
+
+    mat_delete(m);
+    mat_delete(mc);
+
+    return;
+}
 //===========================================================================================
 //===========================================================================================
 
@@ -178,11 +301,17 @@ int main() {
     test_mat_equal();
     test_mat_get_rows();
     test_mat_get_cols();
+    
 
     // mathematics:
     test_mat_row_op1();
     test_mat_row_op2();
     test_mat_row_op3();
+    test_mat_product();
+    test_mat_had_product();
+    test_mat_scale();
+    test_mat_sum();
+    test_mat_transpose();
     
     return 0;
 }
