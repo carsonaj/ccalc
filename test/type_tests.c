a334736ba1146c8760bdebb8c7eff4a929a7fb56
Carson James <carsonaj@stat.tamu.edu>
2021-01-18 15:17:54 -0600

    added test for t_neg, t_equal

--- test/type_tests.c
+++ test/type_tests.c
@@ -7,6 +7,13 @@
 #define FALSE 0
 
 // define tests:
+void test_t_print() {
+    tvalue tval = t_dbl(2.3);
+    t_print(tval);
+
+    return;
+}
+
 void test_t_dbl() {
     tvalue tval = t_dbl(3.2);
 
@@ -42,15 +49,15 @@ void test_t_zero() {
 void test_t_neg() {
     tvalue x = t_dbl(2.3);
     tvalue neg = t_neg(x);
-    assert(neg.val.dblval == -2.3);
+    assert(t_equal(neg, t_dbl(-2.3)) == TRUE);
 
     return;
 }
 
 void test_t_inv() {
-    tvalue x = t_dbl(2.3);
+    tvalue x = t_dbl(2);
     tvalue inv = t_inv(x);
-    assert(inv.val.dblval == -2.3);
+    assert(t_equal(inv, t_dbl(.5)) == TRUE);
 
     return;
 }
@@ -60,7 +67,7 @@ void test_t_equal() {
     tvalue tval2 = t_dbl(2.1);
     tvalue tval3; 
     tval3.type = DBL;
-    tval3.val.dblval = 1.0;
+    tval3.val.dblval = 1.0000000001;
 
     assert(t_equal(tval1, tval2) == FALSE);
     assert(t_equal(tval1, tval3) == TRUE);
@@ -92,9 +99,12 @@ void test_t_product() {
 
 // run tests:
 int main() {
+    // test_t_print();
     test_t_dbl();
     test_t_dbls();
     test_t_zero();
+    test_t_neg();
+    test_t_inv();
     test_t_equal();
     test_t_sum();
     test_t_product();
